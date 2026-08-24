3ed23d9fab6cc93ef19432ca8c20fa26516e9185
ariidesu <shiinazzz.burga@gmail.com>
2026-08-24 15:49:08 +0700

    fix: resolve bass framework on ios

--- BejeweledLivePlus.iOS/Program.cs
+++ BejeweledLivePlus.iOS/Program.cs
@@ -1,4 +1,6 @@
+using System.Runtime.InteropServices;
 using Foundation;
+using ManagedBass;
 using UIKit;
 
 namespace BejeweledLivePlus.iOS
@@ -10,8 +12,15 @@ namespace BejeweledLivePlus.iOS
 
         internal static void RunGame()
         {
+            MapLibraryNames();
             game = new GameMain();
             game.Run();
+        }
+
+        private static void MapLibraryNames()
+        {
+            NativeLibrary.SetDllImportResolver(typeof(Bass).Assembly,
+                (_, assembly, path) => NativeLibrary.Load("@rpath/bass.framework/bass", assembly, path));
         }
 
         static void Main(string[] args)
